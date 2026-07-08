e2442e8a3d34871ff14ac4ab7f92bf668eb4d371
enocgarciadev <enocgarcia2005@gmail.com>
2026-07-07 20:47:45 -0600

    feat(i18n): wire language provider and sync html metadata

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import { useLanguage } from './i18n/useLanguage'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import ValueProposition from './components/ValueProposition'
@@ -6,6 +8,17 @@ import LocationHours from './components/LocationHours'
 import Footer from './components/Footer'
 
 function App() {
+  const { locale, t } = useLanguage()
+
+  useEffect(() => {
+    document.documentElement.lang = locale
+    document.title = t('meta.title')
+    const metaDescription = document.querySelector('meta[name="description"]')
+    if (metaDescription) {
+      metaDescription.setAttribute('content', t('meta.description'))
+    }
+  }, [locale, t])
+
   return (
     <div className="min-h-screen bg-secondary">
       <Navbar />
